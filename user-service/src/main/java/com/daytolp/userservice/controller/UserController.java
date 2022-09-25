8d3b4b4a7341cd39dbdd419ca312988f69da1ec1
juan.ortiz <juan.ortizf@infotec.mx>
2022-09-25 15:03:12 -0500

    se creo cicuirt breaker

--- user-service/src/main/java/com/daytolp/userservice/controller/UserController.java
+++ user-service/src/main/java/com/daytolp/userservice/controller/UserController.java
@@ -4,6 +4,7 @@ import com.daytolp.userservice.entity.User;
 import com.daytolp.userservice.model.Bike;
 import com.daytolp.userservice.model.Car;
 import com.daytolp.userservice.service.UserService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -41,6 +42,7 @@ public class UserController {
         return ResponseEntity.ok(userNew);
     }
 
+    @CircuitBreaker(name = "carsCB" ,fallbackMethod = "fallBackGetCars")
     @GetMapping("/cars/{userId}")
     public ResponseEntity<List<Car>> getCars(@PathVariable Integer userId) {
         User user = userService.getUserById(userId);
@@ -49,7 +51,7 @@ public class UserController {
         List<Car> cars = userService.getCars(userId);
         return ResponseEntity.ok(cars);
     }
-
+    @CircuitBreaker(name = "bikesCB" ,fallbackMethod = "fallBackGetBikes")
     @GetMapping("/bikes/{userId}")
     public ResponseEntity<List<Bike>> getBikes(@PathVariable Integer userId) {
         User user = userService.getUserById(userId);
@@ -59,7 +61,7 @@ public class UserController {
         List<Bike> bikes = userService.getBikes(userId);
         return ResponseEntity.ok(bikes);
     }
-
+    @CircuitBreaker(name = "carsCB" ,fallbackMethod = "fallBackSaveCar")
     @PostMapping("/savecar/{userId}")
     public ResponseEntity<Car> saveCar(@PathVariable Integer userId, @RequestBody Car car) {
         if (userService.getUserById(userId) == null)
@@ -67,18 +69,40 @@ public class UserController {
         Car carNew = userService.saveCar(userId, car);
         return ResponseEntity.status(HttpStatus.CREATED).body(carNew);
     }
-
+    @CircuitBreaker(name = "bikesCB" ,fallbackMethod = "fallBackSaveBike")
     @PostMapping("/savebike/{userId}")
-    public ResponseEntity<Bike> saveCar(@PathVariable Integer userId, @RequestBody Bike bike) {
+    public ResponseEntity<Bike> saveBike(@PathVariable Integer userId, @RequestBody Bike bike) {
         if (userService.getUserById(userId) == null)
             ResponseEntity.notFound().build();
         Bike bikeNew = userService.saveBike(userId, bike);
         return ResponseEntity.status(HttpStatus.CREATED).body(bikeNew);
     }
 
+    @CircuitBreaker(name = "allCB" ,fallbackMethod = "fallBackGetAll")
     @GetMapping("/getAll/{userId}")
     public ResponseEntity<Map<String, Object>> getAllVehicles(@PathVariable("userId") int userId) {
         Map<String, Object> result = userService.getUserAndVehicles(userId);
         return ResponseEntity.ok(result);
     }
+
+    private ResponseEntity<List<Car>> fallBackGetCars(@PathVariable("userId") int userId, RuntimeException e) {
+        return new ResponseEntity("El usuario " + userId + " tiene los coches en el taller", HttpStatus.OK);
+    }
+
+    private ResponseEntity<Car> fallBackSaveCar(@PathVariable("userId") int userId, @RequestBody Car car, RuntimeException e) {
+        return new ResponseEntity("El usuario " + userId + " no tiene dinero para coches", HttpStatus.OK);
+    }
+
+    private ResponseEntity<List<Bike>> fallBackGetBikes(@PathVariable("userId") int userId, RuntimeException e) {
+        return new ResponseEntity("El usuario " + userId + " tiene las motos en el taller", HttpStatus.OK);
+    }
+
+    private ResponseEntity<Car> fallBackSaveBike(@PathVariable("userId") int userId, @RequestBody Bike bike, RuntimeException e) {
+        return new ResponseEntity("El usuario " + userId + "  no tiene dinero para motos", HttpStatus.OK);
+    }
+
+    public ResponseEntity<Map<String, Object>> fallBackGetAll(@PathVariable("userId") int userId, RuntimeException e) {
+        return new ResponseEntity("El usuario " + userId + " tiene los vehículos en el taller", HttpStatus.OK);
+    }
+
 }
