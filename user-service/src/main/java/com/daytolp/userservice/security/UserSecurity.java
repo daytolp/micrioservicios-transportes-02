4f2b71a5a3d23c833d1924f5a2de5fabbfbbcab7
juan.ortiz <juan.ortizf@infotec.mx>
2022-09-26 12:39:19 -0500

    ajuste en configuracion de keycloack en user-service

--- user-service/src/main/java/com/daytolp/userservice/security/UserSecurity.java
+++ user-service/src/main/java/com/daytolp/userservice/security/UserSecurity.java
@@ -3,7 +3,6 @@ package com.daytolp.userservice.security;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -12,8 +11,9 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class UserSecurity extends WebSecurityConfigurerAdapter {
     @Override
-    public void configure(HttpSecurity http) throws Exception {
+    protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests().anyRequest().authenticated()
-                .and().oauth2ResourceServer().jwt();
+                .and()
+                .oauth2ResourceServer().jwt();
     }
 }
